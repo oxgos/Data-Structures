75560d65ad4977a24bdadd72ff0c961ffb55b471
oxgos <122486975@qq.com>
2021-06-20 10:50:20 +0800

    调整双向链表逻辑

--- Linked/LLinkedList.js
+++ Linked/LLinkedList.js
@@ -1,67 +1,66 @@
 class Node {
-	constructor(element) {
-		this.element = element
-		this.prev = null
-		this.next = null
-	}
+  constructor(element) {
+    this.element = element
+    this.prev = null
+    this.next = null
+  }
 }
 
 /*
 	双向链表
 */
 class LLinkedList {
-	constructor() {
-		this.head = new Node('head')
-	}
-	insert(newEle, item) {
-		const newNode = new Node(newEle)
-		const currNode = this.find(item)
-		newNode.prev = currNode
-		// 感觉缺少逻辑
-		// if (currNode.next !== null) {
-		// 	currNode.next.prev = newNode
-		// }
-		newNode.next = currNode.next
-		currNode.next = newNode
-	}
-	remove(element) {
-		const removeNode = this.find(element)
-		if (removeNode.next !== null) {
-			removeNode.prev.next = removeNode.next
-			removeNode.next.prev = removeNode.prev
-			removeNode.next = null
-			removeNode.prev = null
-		}
-	}
-	display() {
-		let currNode = this.head
-		while(currNode.next !== null) {
-			console.log(currNode.next.element)
-			currNode = currNode.next
-		}
-	}
-	dispReverse() {
-		let currNode = this.head
-		currNode = this.findLast()
-		while(currNode.prev !== null) {
-			console.log(currNode.prev.element)
-			currNode = currNode.prev
-		}
-	}
-	find(element) {
-		let currNode = this.head
-		while(this.currNode.element !== element) {
-			currNode = currNode.next
-		}
-		return currNode
-	}
-	findLast() {
-		let currNode = this.head
-		while(currNode.next !== null) {
- 			currNode = currNode.next
-		}
-		return currNode
-	}
+  constructor() {
+    this.head = new Node('head')
+  }
+  insert(newEle, item) {
+    const newNode = new Node(newEle)
+    const currNode = this.find(item)
+    newNode.prev = currNode
+    newNode.next = currNode.next
+    if (currNode.next !== null) {
+      currNode.next.prev = newNode
+    }
+    currNode.next = newNode
+  }
+  remove(element) {
+    const removeNode = this.find(element)
+    if (removeNode.next !== null) {
+      removeNode.prev.next = removeNode.next
+      removeNode.next.prev = removeNode.prev
+      removeNode.next = null
+      removeNode.prev = null
+    }
+  }
+  display() {
+    let currNode = this.head
+    while (currNode.next !== null) {
+      console.log(currNode.next.element)
+      currNode = currNode.next
+    }
+  }
+  dispReverse() {
+    let currNode = this.head
+    currNode = this.findLast()
+    while (currNode.prev !== null) {
+      console.log(currNode.prev.element)
+      currNode = currNode.prev
+    }
+  }
+  find(element) {
+    let currNode = this.head
+    while (this.currNode.element !== element) {
+      currNode = currNode.next
+    }
+    return currNode
+  }
+  findLast() {
+    let currNode = this.head
+    while (currNode.next !== null) {
+      currNode = currNode.next
+    }
+    return currNode
+  }
 }
 
 // test
